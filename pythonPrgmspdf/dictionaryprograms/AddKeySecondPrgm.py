a932b3735f976e0e5813ce51881d286d20e26aac
sangharsharanpise11 <sangharsharanpise408@gmail.com>
2019-04-23 10:57:19 +0530

    Commented in AddKeyPrg

--- pythonPrgmspdf/dictionaryprograms/AddKeySecondPrgm.py
+++ pythonPrgmspdf/dictionaryprograms/AddKeySecondPrgm.py
@@ -2,7 +2,7 @@ mydictionary = {12: "vani", 1: "sreenu", 6: "bunny"}
 mydictionary.update({2: "abc"})
 mydictionary.update({"xyz": "dgjhhgj"})
 mydictionary[2] = "fgh"
-print(mydictionary)
+#print(mydictionary)
 
 d = {12: 94, 6: 91}
 d.update({8: 20})
