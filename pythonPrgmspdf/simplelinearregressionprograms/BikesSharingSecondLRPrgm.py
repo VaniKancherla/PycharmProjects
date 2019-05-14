16d6a9087bbfabf359e38738f470d7eadac89732
sangharsharanpise11 <sangharsharanpise408@gmail.com>
2019-05-14 17:37:40 +0530

    Commited Bike Shared Vs Temperature Simple Linear Regression Program

---
+++ pythonPrgmspdf/simplelinearregressionprograms/BikesSharingSecondLRPrgm.py
@@ -0,0 +1,38 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Bike_Sharing.csv')
+x = dataset.iloc[:, [10]].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=1/2, random_state=0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Bikes getting shared based on the temperature of the day (Training set)')
+plt.xlabel('Temperature')
+plt.ylabel('Bikes Shared')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color='green')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Bikes getting shared based on the temperature of the day (Test set)')
+plt.xlabel('Temperature')
+plt.ylabel('Bikes Shared')
+plt.show()
